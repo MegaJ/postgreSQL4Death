5a90ca5f622193621d8d497561cca052a5df6ba2
MegaJ <ryasurethatschocolateicecream@gmail.com>
2016-08-18 15:28:03 -0500

    CSS mostly to have prettier tables. Mostly taken from sqlFiddle.com

--- app.js
+++ app.js
@@ -48,6 +48,8 @@ app.use(function(req, res, next) {
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
+
+// TODO: A funner 404, look at NUVI exampe
     res.render('error', {
       message: err.message,
       error: err
@@ -59,6 +61,8 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
+
+// TODO: Put the funner 404 here
   res.render('error', {
     message: err.message,
     error: {}
